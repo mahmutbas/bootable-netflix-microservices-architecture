5ddb97e22ac6ac4d9a884431788b6543ea908ee0
mahmut <javaon@yahoo.com>
2018-09-01 21:13:48 +0300

    enable eureka server with 2 replicated instances

--- eureka-server/src/main/java/com/mahmutbas/microservices/eureka/server/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/mahmutbas/microservices/eureka/server/eurekaserver/EurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.mahmutbas.microservices.eureka.server.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication
 {
 
