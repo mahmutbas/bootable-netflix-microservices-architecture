7091a3bf749c23d367d41d7bfc69b8da8f0e24bc
mahmut <javaon@yahoo.com>
2018-09-01 22:48:35 +0300

    customer service get client instance from eureka server and call another service with rest template. also add load balancer

--- client-customer-service/src/main/java/com/mahmutbas/microservices/client/clientcustomerservice/controller/CustomerController.java
+++ client-customer-service/src/main/java/com/mahmutbas/microservices/client/clientcustomerservice/controller/CustomerController.java
@@ -2,8 +2,6 @@ package com.mahmutbas.microservices.client.clientcustomerservice.controller;
 
 import com.mahmutbas.microservices.client.clientcustomerservice.model.Customer;
 import com.mahmutbas.microservices.client.clientcustomerservice.model.CustomerOrder;
-import com.netflix.appinfo.InstanceInfo;
-import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
