eb2eb2b77366a40e4b84b9aac7cddfa8b407b731
mahmut <javaon@yahoo.com>
2018-09-01 21:03:10 +0300

    eureka server init

---
+++ eureka-server/src/main/java/com/mahmutbas/microservices/eureka/server/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.mahmutbas.microservices.eureka.server.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
