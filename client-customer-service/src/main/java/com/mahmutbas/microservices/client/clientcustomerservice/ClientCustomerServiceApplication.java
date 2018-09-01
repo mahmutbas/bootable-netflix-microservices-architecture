68e430917af4d2767417f89bd73ca9bf023693a4
mahmut <javaon@yahoo.com>
2018-09-01 22:11:51 +0300

    client customer service init

---
+++ client-customer-service/src/main/java/com/mahmutbas/microservices/client/clientcustomerservice/ClientCustomerServiceApplication.java
@@ -0,0 +1,14 @@
+package com.mahmutbas.microservices.client.clientcustomerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientCustomerServiceApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ClientCustomerServiceApplication.class, args);
+    }
+}
---
+++ client-customer-service/src/test/java/com/mahmutbas/microservices/client/clientcustomerservice/ClientCustomerServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package com.mahmutbas.microservices.client.clientcustomerservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClientCustomerServiceApplicationTests
+{
+
+    @Test
+    public void contextLoads()
+    {
+    }
+
+}
