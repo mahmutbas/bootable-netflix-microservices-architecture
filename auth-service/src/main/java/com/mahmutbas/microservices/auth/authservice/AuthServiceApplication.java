15a9e73bb5697e013a2e408ada66785e2cca5613
mahmut <javaon@yahoo.com>
2018-09-02 16:49:21 +0300

    auth-service init

---
+++ auth-service/src/main/java/com/mahmutbas/microservices/auth/authservice/AuthServiceApplication.java
@@ -0,0 +1,14 @@
+package com.mahmutbas.microservices.auth.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+}
