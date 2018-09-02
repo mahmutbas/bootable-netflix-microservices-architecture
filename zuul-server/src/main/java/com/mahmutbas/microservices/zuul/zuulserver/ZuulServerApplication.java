eb18325a4be8a6292165a7f2894e1f532abb9b76
mahmut <javaon@yahoo.com>
2018-09-02 16:01:30 +0300

    zuul gateway with 8862 port. All foreign client will access with this gate.

--- zuul-server/src/main/java/com/mahmutbas/microservices/zuul/zuulserver/ZuulServerApplication.java
+++ zuul-server/src/main/java/com/mahmutbas/microservices/zuul/zuulserver/ZuulServerApplication.java
@@ -2,8 +2,12 @@ package com.mahmutbas.microservices.zuul.zuulserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableEurekaClient // zuul also eureka client
+@EnableZuulProxy
 public class ZuulServerApplication
 {
 
@@ -11,4 +15,9 @@ public class ZuulServerApplication
     {
         SpringApplication.run(ZuulServerApplication.class, args);
     }
+
+    // for try
+    //http://localhost:8862/customers/1
+    //http://localhost:8862/customers
+    //http://localhost:8862/customers will get with differend port number. Thanks to Ribbon (load balancer)
 }
