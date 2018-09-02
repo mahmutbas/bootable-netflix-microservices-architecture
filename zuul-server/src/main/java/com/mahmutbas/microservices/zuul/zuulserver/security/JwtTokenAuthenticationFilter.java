8b796fbf6be8ac2fc5d217f61b8f4192f2d8acc2
mahmut <javaon@yahoo.com>
2018-09-02 16:45:53 +0300

    zuul preparation for jwt security

---
+++ zuul-server/src/main/java/com/mahmutbas/microservices/zuul/zuulserver/security/JwtTokenAuthenticationFilter.java
@@ -0,0 +1,76 @@
+package com.mahmutbas.microservices.zuul.zuulserver.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class JwtTokenAuthenticationFilter extends OncePerRequestFilter
+{
+
+    private final JwtConfig jwtConfig;
+
+    public JwtTokenAuthenticationFilter(JwtConfig jwtConfig)
+    {
+        this.jwtConfig = jwtConfig;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException
+    {
+        //getting authentication header
+        String header = httpServletRequest.getHeader(jwtConfig.getHeader());
+
+        //validating header
+        if(header == null || !header.startsWith(jwtConfig.getPrefix())) {
+            filterChain.doFilter(httpServletRequest, httpServletResponse);
+            return;
+        }
+
+        //if not valid, go to next filter.
+
+        //get token
+        String token = header.replace(jwtConfig.getPrefix(), "");
+
+        try {
+
+            // validating token
+            Claims claims = Jwts.parser()
+                    .setSigningKey(jwtConfig.getSecret().getBytes())
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            String username = claims.getSubject();
+            if(username != null) {
+                @SuppressWarnings("unchecked")
+                List<String> authorities = (List<String>) claims.get("authorities");
+
+                // UsernamePasswordAuthenticationToken: A built-in object, used by spring to represent the current authenticated / being authenticated user.
+                // It needs a list of authorities, which has type of GrantedAuthority interface, where SimpleGrantedAuthority is an implementation of that interface
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+                        username, null, authorities.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
+
+                // authenticate the user
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+
+        } catch (Exception e) {
+            //if   the token is expired or any error
+            SecurityContextHolder.clearContext();
+        }
+
+        // go to the next filter in the filter chain
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+
+    }
+}
