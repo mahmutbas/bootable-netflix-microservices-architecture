b68abf9d1cc792235321ecad7f8d0cd2aaba8b64
mahmut <javaon@yahoo.com>
2018-09-02 15:28:45 +0300

    zuul server init

---
+++ zuul-server/src/main/java/com/mahmutbas/microservices/zuul/zuulserver/ZuulServerApplication.java
@@ -0,0 +1,14 @@
+package com.mahmutbas.microservices.zuul.zuulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulServerApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ZuulServerApplication.class, args);
+    }
+}
